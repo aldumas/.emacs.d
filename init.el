1f1ac2bf09ef5aa9cb03b044f4566b2b01c339bc
Adam Dumas <thedumai+adam@gmail.com>
2021-11-16 14:48:34 -0700

    Added neotree

--- init.el
+++ init.el
@@ -27,10 +27,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lua-mode sqlformat php-mode magit)))
+ '(package-selected-packages (quote (neotree lua-mode sqlformat php-mode magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; Neotree settings
+(global-set-key [f8] 'neotree-toggle)
