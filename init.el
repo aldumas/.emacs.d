99a6c2f813ae2453b838a270461b3586c791bd77
Adam Dumas <thedumai+adam@gmail.com>
2021-12-01 08:30:37 -0700

    Conditionally install use-package with package-install.

--- init.el
+++ init.el
@@ -46,12 +46,16 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit neotree use-package))))
+ '(package-selected-packages (quote (evil magit neotree use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
